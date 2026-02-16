b42ad4429309a94d9a4aec57a2a8b14c024ac4a7
christian <duah.marfochristian@gmail.com>
2026-02-16 15:45:29 +0000

    initial

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "includes/server.h"
-#include <iostream>
-
-int main(){
-
-    Server s(8080);
-    s.serve();
-    return 0;
-}
--- source/storage_engine.cpp
+++
@@ -1,9 +0,0 @@
-#include "../includes/storage_engine.h"
-
-void StorageEngine::set(const std::string &key, const std::string &value){
-    store[key] = value;
-}
-
- std::optional<std::string> StorageEngine::get(const std::string & key){
-    return store.at(key);
- }
